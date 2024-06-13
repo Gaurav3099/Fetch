ce38f310d4b75b84f82a11100030c78d51cc989a
Gaurav Sarkar <gaurav.sarkar830@gmail.com>
2024-06-12 23:17:23 -0700

    initial

---
+++ src/main/java/com/example/fetch/Item.java
@@ -0,0 +1,19 @@
+package com.example.fetch;
+
+public class Item {
+    private int listId;
+    private int id;
+    private String name;
+
+    public int getListId() {
+        return listId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
